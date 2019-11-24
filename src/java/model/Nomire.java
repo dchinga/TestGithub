e99e73f0e197894e7adfb2235efa9fcef627ad84
Sabrina Bv <Sabrina Bv@192.168.1.6>
2019-11-23 23:06:45 -0500

    Buenas noches :v

---
+++ src/java/model/Nomire.java
@@ -0,0 +1,33 @@
+
+package model;
+
+public class Nomire {
+    private String hola;
+    private String apruebeme;
+    private String pe;
+
+    public String getHola() {
+        return hola;
+    }
+
+    public void setHola(String hola) {
+        this.hola = hola;
+    }
+
+    public String getApruebeme() {
+        return apruebeme;
+    }
+
+    public void setApruebeme(String apruebeme) {
+        this.apruebeme = apruebeme;
+    }
+
+    public String getPe() {
+        return pe;
+    }
+
+    public void setPe(String pe) {
+        this.pe = pe;
+    }
+    
+}
